3572d8225468ec7821db2ab35f12bd8806960364
JNfeicui-xiaoma <maweikang_1994@163.com>
2016-06-29 10:50:39 +0800

    添加了ViewPager侧滑时颜色渐变效果

--- app/src/main/java/com/feicui/gitdroid/splash/SplashActivity.java
+++ app/src/main/java/com/feicui/gitdroid/splash/SplashActivity.java
@@ -2,15 +2,37 @@ package com.feicui.gitdroid.splash;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import com.feicui.gitdroid.R;
 
+import butterknife.Bind;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
 public class SplashActivity extends AppCompatActivity {
 
+    @Bind(R.id.btnLogin) Button btnLogin;
+    @Bind(R.id.btnEnter) Button btnEnter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+        ButterKnife.bind(this);
+    }
+
+    @OnClick({R.id.btnLogin,R.id.btnEnter})
+    public void onClick(View view){
+        switch (view.getId()){
+            case R.id.btnLogin:
+                Toast.makeText(SplashActivity.this, "login", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.btnEnter:
+                Toast.makeText(SplashActivity.this, "enter", Toast.LENGTH_SHORT).show();
+                break;
+        }
     }
 }
--- app/src/main/java/com/feicui/gitdroid/splash/SplashPagerFragment.java
+++ app/src/main/java/com/feicui/gitdroid/splash/SplashPagerFragment.java
@@ -1,5 +1,7 @@
 package com.feicui.gitdroid.splash;
 
+import android.animation.ArgbEvaluator;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,10 +9,12 @@ import android.support.v4.view.ViewPager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 
 import com.feicui.gitdroid.R;
 
 import butterknife.Bind;
+import butterknife.BindColor;
 import butterknife.ButterKnife;
 import me.relex.circleindicator.CircleIndicator;
 
@@ -23,7 +27,16 @@ public class SplashPagerFragment extends Fragment {
     @Bind(R.id.viewPager)
     ViewPager mViewPager;
     @Bind(R.id.indicator)
-    CircleIndicator mIndicator;
+    CircleIndicator mIndicator;//指示器（下方的小圆点）
+
+    @BindColor(R.color.colorBlue)
+    int colorBlue;
+    @BindColor(R.color.colorGreen)
+    int colorGreen;
+    @BindColor(R.color.colorRed)
+    int colorRed;
+    @Bind(R.id.content)
+    FrameLayout mFrameLayout;
 
     @Nullable
     @Override
@@ -37,14 +50,44 @@ public class SplashPagerFragment extends Fragment {
         super.onViewCreated(view, savedInstanceState);
         ButterKnife.bind(this, view);
 
-        mAdapter=new SplashPagerAdapter(getContext());
+        mAdapter = new SplashPagerAdapter(getContext());
 
         mViewPager.setAdapter(mAdapter);
-
+        mViewPager.addOnPageChangeListener(onPageChangeListener);
         mIndicator.setViewPager(mViewPager);
     }
 
+    private ViewPager.OnPageChangeListener onPageChangeListener = new ViewPager.OnPageChangeListener() {
+        //ARGB取值器
+        final ArgbEvaluator mEvaluator = new ArgbEvaluator();
 
+        @Override
+        public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+            if (position == 0) {
+                //颜色取蓝色和绿色的中间值  偏移量position [0-1]
+                int color = (int) mEvaluator.evaluate(positionOffset, colorBlue, colorGreen);
+                mFrameLayout.setBackgroundColor(color);
+                return;
+            }
+            //从第二个页面到第三个页面
+            if (position==1){
+                int color = (int) mEvaluator.evaluate(positionOffset, colorGreen, colorRed);
+                mFrameLayout.setBackgroundColor(color);
+                return;
+            }
+
+        }
+
+        @Override
+        public void onPageSelected(int position) {
+
+        }
+
+        @Override
+        public void onPageScrollStateChanged(int state) {
+
+        }
+    };
 
 
 }
